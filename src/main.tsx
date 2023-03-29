54e92042c3cf4edccca008488795764c071b394b
Den <potarykindenis@gmail.com>
2023-03-29 17:17:52 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './s1-main/App'
+import reportWebVitals from './reportWebVitals'
+import store from './s2-homeworks/hw10/bll/store'
+import {Provider} from 'react-redux'
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+   <React.StrictMode>
+        {/*для дз 10*/}
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+)
