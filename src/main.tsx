8dace28594f29b19228f1c00c39eeb098d3e3c78
Den <potarykindenis@gmail.com>
2023-03-29 20:23:54 +0300

    Fix All

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './s1-main/App'
-import reportWebVitals from './reportWebVitals'
+
 import store from './s2-homeworks/hw10/bll/store'
 import {Provider} from 'react-redux'
 
